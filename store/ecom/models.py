688d62a15c653978cb77dd17b94c4c4cad393e3a
ghost-mann <iamaob8@protonmail.com>
2024-12-13 10:32:05 +0300

    feat: added product and order tables in models.py

--- store/ecom/models.py
+++ store/ecom/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Category(models.Model):
@@ -33,3 +34,46 @@ class Customer(models.Model):
             return True
 
         return False
+
+class Products(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    price = models.FloatField(null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='uploads/products/', null=True, blank=True)
+
+    @staticmethod
+    def get_products_by_id(ids):
+        return Products.objects.filter(id__in=ids)
+
+    @staticmethod
+    def get_all_products():
+        return Products.objects.all()
+
+    @staticmethod
+    def get_product_by_categoryid(category_id):
+        if category_id:
+            return Products.objects.filter(category_id=category_id)
+        else:
+            return Products.get_all_products()
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Products,
+                                on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField(max_length=50, default='', blank=True)
+    phone = models.CharField(max_length=50, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def placeOrder(self):
+        self.save()
+
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
