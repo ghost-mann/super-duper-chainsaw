0d0a2f0d95a2ebd0ee39a07da3d1734c3a1f843a
ghost-mann <iamaob8@protonmail.com>
2024-12-13 11:48:14 +0300

    feat: registering models in admin.py

--- store/ecom/admin.py
+++ store/ecom/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category
+from .models import Customer
+from .models import Order
+from .models import Products
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Products)
